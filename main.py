7b8dc38b83f7387cf2520cc58aff1cb78d43e1ff
YegorYeryomenko <57726987+YegorYeryomenko@users.noreply.github.com>
2022-03-19 21:05:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,27 @@ from discord.ext import commands
 import requests  # to be able to make http requests
 import json
 
-client = commands.Bot(command_prefix='.')
+client = commands.Bot(command_prefix = '.')
 
-aws = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjRlOWUyNDEzLTkyYWMtNGJmZS1iNmU3LTc1ZTk3ZjJmNzI5ZCIsImlhdCI6MTY0NzcyMjA0Nywic3ViIjoiZGV2ZWxvcGVyLzU4M2RiYjBmLWI3OWYtNjAxOC04YWUzLTg0YmZhNmMzZjIwNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjU0LjIxOC4xNC40OCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.R72B_M7XMPJLzzo_tRU9lqU2ktFWybtXbSHXnrwg4b5eljBNO_zHufLm8dCeJIV410dm1bRemuSauBC0O9LMyg"
+aws = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjlkODhiNzQxLWY4MTEtNGUzNy04NmZkLWIyMTYwODQ0MTVhNiIsImlhdCI6MTY0NzczNTAzNiwic3ViIjoiZGV2ZWxvcGVyLzU4M2RiYjBmLWI3OWYtNjAxOC04YWUzLTg0YmZhNmMzZjIwNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjM0LjIxNC4xMzUuOTMiXSwidHlwZSI6ImNsaWVudCJ9XX0.DWjD73DNVGrNqmoNRGstAfDZ7dGp_fDIDcmycINIHlhB9TjerL6H3A0TNwRAHG7zA3b_5cyTTypDzs4BRxuDNw"
 home = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjE2YTk4MTE5LTA4MDMtNDc5Ni1hNDFhLTgzZmU4YmU2YjE1OSIsImlhdCI6MTY0NzQ3ODM1Niwic3ViIjoiZGV2ZWxvcGVyLzU4M2RiYjBmLWI3OWYtNjAxOC04YWUzLTg0YmZhNmMzZjIwNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjI0Ljg1LjI0Ni45NSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.mrhRIsg3VVn2qmdalA96Wl92iOSHtqvoSvlb4rB341BkiBcmnS_V_NeWDQNkn2zulm0zWUB8kKoCb9z834MfMA"
 
 headers = {
     'authorization': 'Bearer ' + aws,
     'Accept': 'application/json'
-}
-
+}     
 
 def get_player_info(player_tag):
     # return user profile information
     response = requests.get("https://api.clashofclans.com/v1/players/%23" + player_tag, headers=headers)
-    # print(response.status_code)
+    #print(response.status_code)
     return response.json()
 
 
 def get_clan_info(clan_tag="L8VU00CY"):
     # submit a clan search
     response = requests.get("https://api.clashofclans.com/v1/clans/%23" + clan_tag, headers=headers)
-    # print(response.status_code)
+    #print(response.status_code)
     return response.json()
 
 
@@ -49,9 +48,9 @@ Friend in Need           {}'''.format(data['name'],
 
 def clan_info_helper(data):
     if (data['isWarLogPublic'] == False):
-        loss_var = "War Log is set to Private"
-    else:
-        loss_var = data['warLosses']
+        loss_var="War Log is set to Private"
+    else: 
+        loss_var=data['warLosses']
     output_str = "Clan Tag: {tag}\nClan Name: {name}\nType: {type}\nClan Level: {level}\nClan Trophies: {trophies}\nClan Wins: {wins}\nWar Losses: {losses}\nCWL: {league}\n".format(
         tag=data['tag'],
         name=data['name'],
@@ -83,7 +82,7 @@ async def on_message(message):
 .playerinfo <PlayerTag> : gets information about a player with the #PlayerTag
 .claninfo <ClanTag> : gets clan informatin for clan with #ClanTag. 
     ->If .claninfo is called without a ClanTag, the default clan is used''')
-
+        
     if msg.startswith(".playerinfo"):
 
         player_tag = msg.split('.playerinfo ', 1)[1]
@@ -111,6 +110,5 @@ async def on_message(message):
             await msend(output_str)
         except Exception:
             await msend("Something went wrong")
-
 
 client.run('OTUzNzA3MjcyMTQyNDcxMjA4.YjIfHA.PMT9g77nRgi7qTRMO0kwKbTPUBs')
